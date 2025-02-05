bd4daefa8a25dd35129334cf5ec6e9cd467e6013
zEL-PORCELiE <zEL-PORCELiE@users.noreply.github.com>
2025-02-05 11:35:05 +0000

    chore: update main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project name="fijitimes-spider">
+    <description>Configuration for fijitimes-spider automated tasks</description>
+    <files>
+        <file name="main" type="source"/>
+        <file name="README.md" type="doc"/>
+        <file name="setup.cfg" type="config"/>
+    </files>
+    <dependencies>
+        <dependency name="requests" version="2.31.0"/>
+        <dependency name="aiohttp" version="3.9.0"/>
+    </dependencies>
+    <settings>
+        <env>production</env>
+        <debug>false</debug>
+        <retry>3</retry>
+    </settings>
+</project>
